74822de0fe8777e787db3c16a8079e6b995ff59f
Pabel <radowan.bhuiyan.tri@gmail.com>
2021-09-21 16:03:43 +0600

    in office dashboard created

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'screens/welcome_screen.dart';
 import 'screens/analytics_screen.dart';
+import 'screens/dashboard_screen.dart';
 
 class AppRouter {
   Route generateRoute(RouteSettings routeSettings) {
     switch (routeSettings.name) {
       case "/":
-        return MaterialPageRoute(builder: (_) => AnalyticsScreen());
+        return MaterialPageRoute(builder: (_) => DashboardScreen());
+      // case "/":
+      //   return MaterialPageRoute(builder: (_) => AnalyticsScreen());
+      // case "/dashboard":
+      //   return MaterialPageRoute(builder: (_) => DashboardScreen());
       default:
         return MaterialPageRoute(builder: (_) => WelcomeScreen());
     }
